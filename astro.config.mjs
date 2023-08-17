6dd076e884c6640b977eb46fb3bae4b21811a4b4
Cícero José <49200311+c1c3ro@users.noreply.github.com>
2023-08-17 17:51:29 -0300

    lead-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://c1c3ro.github.io',
+  integrations: []
 });
-
---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const WHATSAPP_LINK = 'https://api.whatsapp.com/send/?phone=558821319977&text=Ol%C3%A1%2C+gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre%3A';
+export const VIDEO_URL = 'https://www.youtube.com/embed/rsvCduomUA8';
+export const VIDEO_IMAGE = 'https://cdn.nad-edu.com.br/wp-content/uploads/2023/06/Capa-Youtube-1.webp';
